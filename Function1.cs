9b55e7db62bc16adcfe8e9104f8da2391be153bd
W0496269 <W0496269@campus.nscc.ca>
2025-04-09 22:07:37 -0300

    Add project files.

---
+++ Function1.cs
@@ -0,0 +1,78 @@
+using System;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Microsoft.Data.SqlClient;
+
+namespace TicketFunction
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Function1))]
+        public async Task Run([QueueTrigger("tickethub", Connection = "AzureWebJobsStorage")] QueueMessage message)
+        {
+            _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+
+            string messageJson = message.MessageText;
+
+            // Deserialize the
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            Ticket? ticket = JsonSerializer.Deserialize<Ticket>(messageJson, options);
+
+            if (ticket == null)
+            {
+                _logger.LogError("Failed to deserialize the message.");
+                return;
+            }
+            _logger.LogInformation($"Ticket details: {ticket.ConcertId}, {ticket.Email}, {ticket.Name}, {ticket.Phone}, {ticket.Quantity}, {ticket.CreditCard}, {ticket.Expiration}, {ticket.SecurityCode}, {ticket.Address}, {ticket.City}, {ticket.Province}, {ticket.PostalCode}, {ticket.Country}");
+
+            // Add Ticket to database
+
+            // get connection string from app settings
+            string? connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("SQL connection string is not set in the environment variables.");
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync(); // Note the ASYNC
+
+                var query = "INSERT INTO Tickets (ConcertId, Email, Name, Phone, Quantity, CreditCard, Expiration, SecurityCode, Address, City, Province, PostalCode, Country) VALUES (@ConcertId, @Email, @Name, @Phone, @Quantity, @CreditCard, @Expiration, @SecurityCode, @Address, @City, @Province, @PostalCode, @Country);";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ConcertId", ticket.ConcertId);
+                    cmd.Parameters.AddWithValue("@Email", ticket.Email);
+                    cmd.Parameters.AddWithValue("@Name", ticket.Name);
+                    cmd.Parameters.AddWithValue("@Phone", ticket.Phone);
+                    cmd.Parameters.AddWithValue("@Quantity", ticket.Quantity);
+                    cmd.Parameters.AddWithValue("@CreditCard", ticket.CreditCard);
+                    cmd.Parameters.AddWithValue("@Expiration", ticket.Expiration);
+                    cmd.Parameters.AddWithValue("@SecurityCode", ticket.SecurityCode);
+                    cmd.Parameters.AddWithValue("@Address", ticket.Address);
+                    cmd.Parameters.AddWithValue("@City", ticket.City);
+                    cmd.Parameters.AddWithValue("@Province", ticket.Province);
+                    cmd.Parameters.AddWithValue("@PostalCode", ticket.PostalCode);
+                    cmd.Parameters.AddWithValue("@Country", ticket.Country);
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+
+        }
+    }
+}
---
+++ Ticket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketFunction
+{
+    public class Ticket
+        {
+        [Required]
+        public int ConcertId { get; set; } 
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, Phone]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required, Range(1, 10)]
+        public int Quantity { get; set; }
+
+        [Required, CreditCard]
+        public string CreditCard { get; set; } = string.Empty;
+
+        [Required]
+        public string Expiration { get; set; } // MM/YY format
+
+        [Required, StringLength(3, MinimumLength = 3)] 
+        public string SecurityCode { get; set; } = string.Empty;
+
+        [Required]
+        public string Address { get; set; } 
+
+        [Required]
+        public string City { get; set; } 
+
+        [Required]
+        public string Province { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+    
+    }
+}
